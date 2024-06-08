7529c2a0ccd542a845f799934994ebca865f84b2
Clansty <i@gao4.pw>
2024-06-08 20:28:07 +0800

    移植官方读卡协议

--- CardReader/src/main.cpp
+++ CardReader/src/main.cpp
@@ -1,18 +1,5 @@
 #include <Arduino.h>
-
-#include <Wire.h>
-#include <PN532_I2C.h>
-#include <PN532.h>
-#include <FastLED.h>
-#include <types.h>
-
-constexpr u8 NUM_LEDS = 7;
-constexpr u8 BR_DIM = 8;
-constexpr u8 BR_BRIGHT = 14;
-CRGB leds[NUM_LEDS];
-
-PN532_I2C pn532i2c(Wire);
-PN532 nfc(pn532i2c);
+#include "Aime_Reader.h"
 
 constexpr u8 UID_LENGTH = 8;
 u8 prevIDm[UID_LENGTH];
@@ -22,10 +9,10 @@ void setup()
 {
     // Add initial delay to allow the serial monitor to catch up
     delay(500);
+    // led_animation();
 
     // Initialize serial port
     USBSerial.begin(115200);
-    USBSerial.println("Hello!");
 
     // Initialize I2C communication
     Wire.setPins(GPIO_NUM_4, GPIO_NUM_5);
@@ -43,21 +30,14 @@ void setup()
         delay(100);
     }
 
-    // Got ok data, print it out!
-    USBSerial.print("Found chip PN5");
-    USBSerial.println(versiondata >> 24 & 0xFF, HEX);
-    USBSerial.print("Firmware ver. ");
-    USBSerial.print(versiondata >> 16 & 0xFF, DEC);
-    USBSerial.print('.');
-    USBSerial.println(versiondata >> 8 & 0xFF, DEC);
-
     // Set the max number of retry attempts to read from a card
     // This prevents us from waiting forever for a card, which is the default behaviour of the PN532.
-    nfc.setPassiveActivationRetries(0xFF);
+    nfc.setPassiveActivationRetries(0x10);
     nfc.SAMConfig();
 
     // Clear the IDm buffer
-    memset(prevIDm, 0, UID_LENGTH);
+    memset(req.bytes, 0, sizeof(req.bytes));
+    memset(res.bytes, 0, sizeof(res.bytes));
 }
 
 void led_animation()
@@ -78,7 +58,20 @@ void led_animation()
     FastLED.setBrightness(BR_DIM);
 }
 
-void foundCard(const u8* uid, const u8 len, const char* cardType)
+void led_animation(CRGB color)
+{
+    FastLED.setBrightness(BR_BRIGHT);
+    for (u8 i = 1; i < NUM_LEDS; i++)
+    {
+        leds[i] = color;
+        FastLED.show();
+        delay(100);
+    }
+
+    FastLED.setBrightness(BR_DIM);
+}
+
+void foundCard(const u8 *uid, const u8 len, const char *cardType)
 {
     // Check if the same card is present
     if (memcmp(uid, prevIDm, UID_LENGTH) == 0 && millis() - prevTime < 3000)
@@ -89,8 +82,10 @@ void foundCard(const u8* uid, const u8 len, const char* cardType)
 
     USBSerial.printf("\nFound a %s card!\n", cardType);
     USBSerial.print("UID Value: ");
-    for (u8 i = 0; i < len; i++) {
-        if (uid[i] < 0x10) USBSerial.print("0");
+    for (u8 i = 0; i < len; i++)
+    {
+        if (uid[i] < 0x10)
+            USBSerial.print("0");
         USBSerial.print(uid[i], HEX);
     }
     USBSerial.println("");
@@ -103,21 +98,75 @@ void foundCard(const u8* uid, const u8 len, const char* cardType)
 
 void loop()
 {
-    u8 idm[UID_LENGTH] = {0};
-    u8 pmm[8];
-    u16 systemCode;
+    switch (packet_read())
+    {
+    case 0:
+        break;
 
-    // Wait for an FeliCa type cards.
-    // When one is found, some basic information such as IDm, PMm, and System Code are retrieved.
-    leds[0] = CRGB::BlueViolet;
-    FastLED.show();
-    if (nfc.felica_Polling(0xFFFF, 0x00, idm, pmm, &systemCode, 5) == 1)
-        foundCard(idm, UID_LENGTH, "FeliCa");
+    case CMD_TO_NORMAL_MODE:
+        sys_to_normal_mode();
+        break;
+    case CMD_GET_FW_VERSION:
+        sys_get_fw_version();
+        break;
+    case CMD_GET_HW_VERSION:
+        sys_get_hw_version();
+        break;
 
-    // Wait for an ISO14443A type cards (MIFARE, etc.).  When one is found
-    u8 uidLength;
-    leds[0] = CRGB::OrangeRed;
-    FastLED.show();
-    if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, idm, &uidLength, 5) == 1)
-        foundCard(idm, uidLength, "ISO14443A");
+    // Card read
+    case CMD_START_POLLING:
+        nfc_start_polling();
+        led_animation();
+        break;
+    case CMD_STOP_POLLING:
+        nfc_stop_polling();
+        break;
+    case CMD_CARD_DETECT:
+        nfc_card_detect();
+        break;
+
+    // MIFARE
+    case CMD_MIFARE_KEY_SET_A:
+        memcpy(KeyA, req.key, 6);
+        res_init();
+        break;
+
+    case CMD_MIFARE_KEY_SET_B:
+        res_init();
+        memcpy(KeyB, req.key, 6);
+        break;
+
+    case CMD_MIFARE_AUTHORIZE_A:
+        nfc_mifare_authorize_a();
+        break;
+
+    case CMD_MIFARE_AUTHORIZE_B:
+        nfc_mifare_authorize_b();
+        break;
+
+    case CMD_MIFARE_READ:
+        nfc_mifare_read();
+        break;
+
+    // FeliCa
+    case CMD_FELICA_THROUGH:
+        nfc_felica_through();
+        break;
+
+    // LED
+    case CMD_EXT_BOARD_LED_RGB:
+        FastLED.showColor(CRGB(req.color_payload[0], req.color_payload[1], req.color_payload[2]));
+        break;
+
+    case CMD_EXT_BOARD_INFO:
+        sys_get_led_info();
+        break;
+
+    case CMD_EXT_BOARD_LED_RGB_UNKNOWN:
+        break;
+
+    default:
+        res_init();
+    }
+    packet_write();
 }
